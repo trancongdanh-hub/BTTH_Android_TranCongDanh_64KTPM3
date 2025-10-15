27ce7627150b35216afcb479da16cac159868c02
danhtrancong-hub <danhtrancong2004@gmail.com>
2025-10-15 19:14:05 +0700

    nop bt labb1

--- lab3_1/android/app/src/main/kotlin/com/example/lab3_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lab3_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- tlu_firebase/android/app/src/main/kotlin/com/example/tlu_firebase/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tlu_firebase
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
